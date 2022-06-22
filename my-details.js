10d575bc64c73758e681f8f420e143cbed315e9b
olowu abayomi <aayobam@gmail.com>
2022-06-22 01:59:33 +0100

    first code

---
+++ my-details.js
@@ -0,0 +1,9 @@
+function userDetails(){
+    const name = "OLOWU ABAYOMI"
+    const height = "5'7 inches"
+    const country = "Ngeria"
+    console.log("Name = " + name)
+    console.log("Height = " + height)
+    console.log("Country = " + country )
+};
+userDetails();
